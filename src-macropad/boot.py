925b38a08352f86d6ff9f23d88336787cf49f7fe
Deby <44121160+DebyGe@users.noreply.github.com>
2025-03-13 12:14:09 +0100

    Boot Enable - Dsable GP6 USB drive

--- src-macropad/boot.py
+++ src-macropad/boot.py
@@ -1,3 +1,16 @@
+import board
+import digitalio
 import storage
 
-storage.disable_usb_drive()
+noStorage = False
+noStoragePin = digitalio.DigitalInOut(board.GP6)
+noStoragePin.direction = digitalio.Direction.INPUT
+noStoragePin.pull = digitalio.Pull.DOWN
+noStorage = noStoragePin.value
+
+if(noStorage == True):
+    print("USB drive  enabled")
+else:
+    # normal boot
+    storage.disable_usb_drive()
+    print("USB drive disable")
